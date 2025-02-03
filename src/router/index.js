4007d70d8641fd16cd1a3cd93770e33e5a18fa7b
Zheng Qihui <teikie@akane.waseda.jp>
2025-02-04 01:25:00 +0900

    some revisement and update

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,17 @@ import Layout from "@/pages/Layout";
 import Month from "@/pages/Month";
 import New from "@/pages/New";
 import Year from "@/pages/Year";
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
         children:[
+            {
+                path: '/', 
+                element: <Navigate to="/month" replace />
+            },
             {
                 path: 'month',
                 element: <Month />
